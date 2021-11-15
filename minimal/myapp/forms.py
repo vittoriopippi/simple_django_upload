5c04aec63b634c1905f4fbb2c4457a6ed4c33140
Vittorio Pippi <219424@studenti.unimore.it>
2021-11-15 12:27:16 +0100

    Minor changes

--- minimal/myapp/forms.py
+++ minimal/myapp/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class DocumentForm(forms.Form):
     name = forms.CharField(label='Nome', max_length=100)
     surename = forms.CharField(label='Cognome', max_length=100)
-    classe = forms.CharField(label='Classe', max_length=100)
+    classe = forms.CharField(label='Gruppo', max_length=100)
     docfile = forms.FileField(label='Seleziona il video')
